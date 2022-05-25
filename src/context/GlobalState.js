905bcab91f551b4678e65ff24af8cd0b0e105093
Siyovush <paraparadox@protonmail.com>
2022-05-26 00:29:20 +0500

    Remove json server and fetching data from API

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useReducer } from "react";
+import { createContext, useReducer } from "react";
 import AppReducer from "./AppReducer";
 
 // Initial State
@@ -12,27 +12,6 @@ export const GlobalContext = createContext(initialState);
 // Provider Component
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
-
-  useEffect(() => {
-    const getTransactions = async () => {
-      const transactions = await fetchTransactions()
-      
-      dispatch({
-        type: 'FETCH_TRANSACTIONS',
-        payload: transactions,
-      });
-    }
-
-    getTransactions()
-  }, []);
-
-  // Fetch Transactions
-  const fetchTransactions = async () => {
-    const res = await fetch('http://localhost:5000/transactions')
-    const transactions = await res.json()
-
-    return transactions
-  }
 
   // Add Action
   function addTransaction(transaction) {
